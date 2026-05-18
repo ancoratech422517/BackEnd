13d74c3ac04e0aac3f4f0e48e64c4b230f3fea3c
ancoratech422517 <ancoratech422517@gmail.com>
2026-05-18 22:46:56 +0100

    oi
    ola

---
+++ backend/routes/websocket.py
@@ -0,0 +1,20 @@
+# routes/websocket.py
+from flask_socketio import SocketIO
+URL_FRONTEND_ANCORA = "http://192.168.11.1:5173"
+socketio = SocketIO(
+    cors_allowed_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "http://127.0.0.1:5173",
+        "http://127.0.0.1",
+        "http://10.140.176.115:5173",
+        "http://10.140.176.115",
+        "http://10.140.176.115:5000",
+        URL_FRONTEND_ANCORA,
+
+    ],
+    cookie='io',  # MUDOU: True -> 'io' ou remove a linha toda
+    async_mode='threading',
+    logger=True,
+    engineio_logger=True
+)
