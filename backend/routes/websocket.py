79bf788148d735934af8fc4cecbe321527bfcd49
ancoratech422517 <ancoratech422517@gmail.com>
2026-05-19 23:16:58 +0100

    Update websocket.py

--- backend/routes/websocket.py
+++ backend/routes/websocket.py
@@ -10,6 +10,6 @@ socketio = SocketIO(
         "http://127.0.0.1:5173",
         "http://192.168.11.1:5173",
         "http://10.140.176.115:5173",
-        "https://loja-taupe-psi.vercel.app"
+        "https://ecomerce-penelaka.netlify.app"
     ]
 )
