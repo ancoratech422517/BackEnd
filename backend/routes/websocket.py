a9baa7340ed942d42b9da0d8ad12c68fa3120aa6
ancoratech422517 <ancoratech422517@gmail.com>
2026-05-19 01:57:26 +0100

    Update websocket.py

--- backend/routes/websocket.py
+++ backend/routes/websocket.py
@@ -1,21 +1,15 @@
 # routes/websocket.py
 from flask_socketio import SocketIO
-URL_FRONTEND_ANCORA = "https://ancoras.netlify.app"
-socketio = SocketIO(
-    cors_allowed_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "http://127.0.0.1:5173",
-        "http://127.0.0.1",
-        "http://10.140.176.115:5173",
-        "http://10.140.176.115",
-        "http://10.140.176.115:5000",
-        URL_FRONTEND_ANCORA,
 
-    ],
-    cookie='io',  # MUDOU: True -> 'io' ou remove a linha toda
-    async_mode='threading',
+socketio = SocketIO(
     logger=True,
     engineio_logger=True,
-    async_mode='gevent'
+    async_mode='gevent',
+    cors_allowed_origins=[          # CORRIGIDO: lista explícita em vez de "*"
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "http://192.168.11.1:5173",
+        "http://10.140.176.115:5173",
+        "https://ancoras.netlify.app"
+    ]
 )
